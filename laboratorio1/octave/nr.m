18cb5a4b999af577f4e6564941140c898c8ef58d
Alvaro Badaracco <alvaro.badaracco@mercadolibre.com>
2019-09-07 15:46:57 -0300

    Added new NR

--- laboratorio1/octave/nr.m
+++ laboratorio1/octave/nr.m
@@ -1,12 +1,10 @@
-function xSol = nr(b0, iter)
+function xSol = nr(xn, iter)
   
-  currentB = -ecuacion15(b0);
-  A = [fila1(b0);fila2(b0)];
-
   for i = 1:iter
-    x = currentB - A \ currentB';
-    currentB = -ecuacion15(x);  
-    A = [fila1(x);fila2(x)];
+    fx = -ecuacion15(xn);
+    J = [fila1(xn);fila2(xn)];
+    di = J \fx';
+    xn = [di(1) + xn(1), di(2) + xn(2)];  
   end
-  xSol = x
+  xSol = xn
 endfunction

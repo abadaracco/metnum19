55088e20fd138294e3685a816adddea88c7e88ac
Alvaro Badaracco <alvaro.badaracco@mercadolibre.com>
2019-09-07 17:37:23 -0300

    Added final NR

---
+++ laboratorio1/octave/F.m
@@ -0,0 +1,4 @@
+function [f] = F(x)
+  f(1) = x(1) +1 + e^(x(1)+x(2));
+  f(2) = -x(1)/3 + 3*x(2) -2 + e^(x(1)+x(2));
+endfunction
--- laboratorio1/octave/nr.m
+++ laboratorio1/octave/nr.m
@@ -1,10 +1,26 @@
-function xSol = nr(xn, iter)
+function [xSol, valx, valy, nfi] = nr(xn, iter)
   
   for i = 1:iter
     fx = -ecuacion15(xn);
     J = [fila1(xn);fila2(xn)];
     di = J \fx';
-    xn = [di(1) + xn(1), di(2) + xn(2)];  
+    #resultados intermedios para la parte 3.6
+    valx(i) = xn(1);
+    valy(i) = xn(2);
+    nfi(i) = norm(F(xn));
+    #fin intermedios
+    xn = [di(1) + xn(1), di(2) + xn(2)]; 
   end
   xSol = xn
+  valx = valx
+  valy = valy
+  nfi = nfi
+  #para graficar los valores
+  rango = 1:iter;
+  figure
+  hold on
+  plot(rango, nfi)
+  ylabel(' ||F(z_k)||')
+  xlabel('# iteración')
+  hold off
 endfunction
